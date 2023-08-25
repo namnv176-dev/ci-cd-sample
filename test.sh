97d3522e7f7311c87db8952b63ca35e7c57d3ae2
namnv176 <nguyen.van.nam-e@sun-asterisk.com>
2023-08-25 13:33:22 +0700

    create bash script

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+commit_hash=""
 # String to split
 input_string="sync-branch,test-sync1,test-sync2,test-sync3"
 
@@ -5,7 +6,17 @@ input_string="sync-branch,test-sync1,test-sync2,test-sync3"
 IFS="," read -r -a arr <<< "${input_string}"
 
 # Loop through the array elements
-for element in "${arr[@]}"
+for element in "${branches[@]}"
 do
-    echo "$element"
+    echo "Start sync branch: $element"
+    git checkout "$element"
+    git cherry-pick -m 1 "$commit_hash" >/dev/null 2>&1
+    # Check if cherry-pick was successful
+    if [ $? -eq 0 ]; then
+        git push origin "$element"
+        echo "Success sync branch $element!"
+    else
+        git cherry-pick --abort
+        echo "Failed sync branch $element!"
+    fi
 done
